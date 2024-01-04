5386363fef36a7d3c642463737c95ad356669ead
Etienne Reboul <reboul@ibpc.fr>
2024-01-04 17:50:59 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ClearSMILES is a data augmentation procedure for SMILES. The goal of CLearSMILES is to minimize the dimensionality of SMILES and their semantic complexity',
+    author='Etienne_Reboul',
+    license='MIT',
+)
